80fa5da09e11df8338226cf29d66643885b6195d
AmalAlby <amalalbyn@gmail.com>
2022-12-07 16:08:30 +0530

    Search added

--- src/main/java/com/nest/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/nest/courseapp_backend/controller/CourseController.java
@@ -5,7 +5,9 @@ import com.nest.courseapp_backend.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CourseController {
@@ -20,15 +22,25 @@ public class CourseController {
     }
 
     @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes ="application/json",produces ="application/json")
+    public List<Course> Search_course(@RequestBody Course c){
+        String c_title=c.getCourseTitle().toString();
+        System.out.println(c_title);
+        return (List<Course>) dao.SearchCourse(c.getCourseTitle());
+    }
+
+    @CrossOrigin(origins = "*")
     @PostMapping(path="/add",consumes ="application/json" ,produces = "application/json")
-    public String add(@RequestBody Course c){
+    public Map<String,String> add(@RequestBody Course c){
         System.out.println("Course Title = "+c.getCourseTitle().toString());
         System.out.println("Course  Description = "+c.getCourseDesc().toString());
         System.out.println("Course Venue = "+c.getVenue().toString());
         System.out.println("Course Duration = "+c.getDuration().toString());
         System.out.println("Course Date = "+c.getDate().toString());
         dao.save(c);
-        return "COURSE ADDED SUCCESSFULLY";
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
     }
 
     @CrossOrigin(origins = "*")
